3d1b7070a207d5fdc90d875efa0178560ff21808
Jon Bardin <diclophis@gmail.com>
2018-08-02 12:11:32 -0700

    working demo

--- lib/init.rb
+++ lib/init.rb
@@ -4,48 +4,46 @@ ENV={}
 
 gl = GameLoop.new("kit1zx", 512, 512, 61)
 
-building = Model.new("resources/hqalt1.obj", "resources/hqalt1tex.png", 1333.0)
+hq = Model.new("resources/hqalt1.obj", "resources/hqalt1tex.png", 1333.0)
 tunnel = Model.new("resources/seqalt3.obj", "resources/seqtex.png", 1111.0)
 player_one = Model.new("resources/playerone001.obj", "resources/playerone.png", 0.5)
 
-tr = 0.0
-tp = 0.0
-bp = -3456
-tp = -800
+tunnel_rotation = 0.0
+hq_position = -3456
+tunnel_position = -800
 
-lx = 0.0
+last_x = 0.0
 
 gl.main_loop { |gt, dt|
-  #gl.draw_grid(100, 0.1)
+  gl.draw_grid(100, 0.1)
 
-  tr += 0.125
+  tunnel_rotation += 0.125
+  tunnel_position += 5.33
+  hq_position += 5.00
 
-  tp += 5.33
-  bp += 5.00
-
-  if tp > 2000
-    tp = -800
+  if tunnel_position > 2000
+    tunnel_position = -800
   end
 
-  if bp > 1000
-    bp = -3456
+  if hq_position > 1000
+    hq_position = -3456
   end
 
-  building.deltap(0.0, 0.0, -bp)
-  building.yawpitchroll(0.0, 0.0, tr * -0.5)
+  hq.deltap(0.0, 0.0, -hq_position)
+  hq.yawpitchroll(0.0, 0.0, tunnel_rotation * -0.5)
 
-  tunnel.deltap(0.0, 0.0, -tp)
-  tunnel.yawpitchroll(0.0, 0.0, tr)
+  tunnel.deltap(0.0, 0.0, -tunnel_position)
+  tunnel.yawpitchroll(0.0, 0.0, tunnel_rotation)
 
   gl.mousep { |xyz|
     x,y,z = xyz
-    ndx = (lx - x) * 100.0
+    ndx = (last_x - x) * 100.0
     player_one.yawpitchroll(0.0, 0.0, -(ndx))
     player_one.deltap(x, 0.0, z)
-    lx = x
+    last_x = x
   }
 
-  building.draw
+  hq.draw
   tunnel.draw
   player_one.draw
 
