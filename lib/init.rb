3c0ba263c5770c465f51adc6456c61356bbdfd16
Jon Bardin <diclophis@gmail.com>
2018-08-03 03:16:29 -0700

    more models

--- lib/init.rb
+++ lib/init.rb
@@ -4,13 +4,16 @@ ENV={}
 
 gl = GameLoop.new("kit1zx", 512, 512, 60)
 
-hq = Model.new("resources/hqalt1.obj", "resources/hqalt1tex.png", 1333.0)
-tunnel = Model.new("resources/seqalt3.obj", "resources/seqtex.png", 1111.0)
-player_one = Model.new("resources/playerone001.obj", "resources/playerone.png", 100.0)
+player_scale = 13.0
+
+hq = Model.new("resources/hqbits.obj", "resources/hqbits.png", 1000.0)
+tunnel = Model.new("resources/seqalt3.obj", "resources/seqtex.png", 1000.0)
+player_one = Model.new("resources/playerone001.obj", "resources/playerone.png", player_scale)
 
 tunnel_rotation = 0.0
-hq_position = -3456
-tunnel_position = -800
+hq_position = 0
+tunnel_position = 0
+tunnel_position_x = 0
 
 last_x = 0.0
 last_z = 0.0
@@ -32,29 +35,30 @@ last_roll = 0.0
 time_at_vector = 0.0
 
 gl.main_loop { |gtdt|
-  global_time = gtdt[0]
-  delta_time = gtdt[1]
+  global_time, delta_time = gtdt
 
   next unless delta_time > 0.0
 
   #gl.draw_grid(100, 0.1)
 
-  tunnel_rotation += 50.0 * delta_time
-  tunnel_position += 100.0 * delta_time
-  hq_position += 30.00 * delta_time
+  tunnel_rotation += 17.0 * delta_time
+  tunnel_position += 333.0 * delta_time
 
-  if tunnel_position > 2000
-    tunnel_position = -800
+  hq_position += 33.00 * delta_time
+
+  if tunnel_position > 1500
+    tunnel_position = -1500
+    tunnel_position_x = (rand * 500.0) - 250.0
   end
 
-  if hq_position > 1000
-    hq_position = -3456
+  if hq_position > 1500
+    hq_position = -1500
   end
 
-  hq.deltap(0.0, 0.0, -hq_position)
-  hq.yawpitchroll(0.0, 0.0, tunnel_rotation * -0.5)
+  hq.deltap(0.0, -1000.0, -hq_position)
+  hq.yawpitchroll(0.0, tunnel_rotation * 0.025, tunnel_rotation * -0.05)
 
-  tunnel.deltap(0.0, 0.0, -tunnel_position)
+  tunnel.deltap(tunnel_position_x, -250.0, -tunnel_position)
   tunnel.yawpitchroll(0.0, 0.0, tunnel_rotation)
 
   gl.mousep { |xyz|
@@ -73,22 +77,31 @@ gl.main_loop { |gtdt|
     #else
     #  time_at_vector = 0.0
     #end
-    time_at_vector = 0.125
 
-    max_accel = 0.05
+    #time_at_vector = 0.001
+    #accel_met = 0.001
+
+    max_accel = (0.5 * player_scale)
+    max_velocity = (0.5 * player_scale)
+
     if left_right_accel > max_accel
       left_right_accel = max_accel
     elsif left_right_accel < -max_accel
       left_right_accel = -max_accel
     end
 
-    puts left_right_accel
+    if left_right_velocity > max_velocity
+      left_right_velocity = max_velocity
+    elsif left_right_velocity < -max_velocity
+      left_right_velocity = -max_velocity
+    end
 
     mx_roll = 45.0
 
-    suggest_new_roll = ((((left_right_accel * 0.025) + (-left_right_velocity * (time_at_vector * 10.0)))) * mx_roll)
+    #suggest_new_roll = ((((left_right_accel * 0.025) + (-left_right_velocity * (time_at_vector)))) * mx_roll)
+    suggest_new_roll = ((left_right_velocity / max_velocity) * mx_roll)
 
-    new_roll = last_roll - ((last_roll - suggest_new_roll) * delta_time * 10.0)
+    new_roll = last_roll - ((last_roll + suggest_new_roll) * delta_time * 10.0)
 
     if new_roll > mx_roll
       new_roll = mx_roll
