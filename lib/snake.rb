66957521e760608f3aa36bce11c1ef19643d033e
Jon Bardin <diclophis@gmail.com>
2018-08-08 02:27:07 -0700

    more cleanup and constantizing

--- lib/snake.rb
+++ lib/snake.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/env ruby
 
 def snake(gl)
-  player = Cube.new(10.0, 10.0, 10.0, 1.0)
-
-  snake = Sphere.new(5.0, 10, 10, 1.0)
-
-  time_it_takes_to_move = 0.234
+  time_it_takes_to_move = 0.34
   time_into_current_move = 0.0
   player_position = [0.0, 0.0, 0.0]
   camera_desired_target = [0.0, 0.0, 0.0]
-  camera_current_target = [10.0, 10.0, 10.0]
+  camera_current_target = [33.0, 33.0, 330.0]
   camera_speed = 4.32
   move_vector = nil
-  
+
+  size = 10.0
+  half_size = size / 2.0
+
+  player = Cube.new(size, size, size, 1.0)
+  snake = Sphere.new(half_size, 10, 10, 1.0)
+
   gl.main_loop { |gtdt|
     global_time, delta_time = gtdt
 
@@ -22,13 +24,13 @@ def snake(gl)
 
     if !move_vector && arrow_keys[0]
       if arrow_keys[0] == KEY_UP
-        move_vector = [0.0, 10.0]
+        move_vector = [0.0, size]
       elsif arrow_keys[0] == KEY_DOWN
-        move_vector = [0.0, -10.0]
+        move_vector = [0.0, -size]
       elsif arrow_keys[0] == KEY_RIGHT
-        move_vector = [-10.0, 0.0]
+        move_vector = [-size, 0.0]
       elsif arrow_keys[0] == KEY_LEFT
-        move_vector = [10.0, 0.0]
+        move_vector = [size, 0.0]
       end
     end
 
@@ -77,7 +79,7 @@ def snake(gl)
           gl.lookat(1, -100.0, 50.0, -99.0, camera_current_target[0], camera_current_target[1], camera_current_target[2], 33.0)
 
         when 1
-          gl.lookat(1, 0.0, 5.0, -99.0, next_player_positionx, 0.0, next_player_positionz, 33.0)
+          gl.lookat(1, 0.0, 13.0, -99.0, next_player_positionx, 0.0, next_player_positionz, 33.0)
 
         when 2
           gl.lookat(0, 0.0, 2000.0, 0.0, 0.0, 0.0, 1.0, 180.0)
@@ -87,13 +89,13 @@ def snake(gl)
 
       if move_vector
         if move_vector[0] > 0
-          player.yawpitchroll(0.0, 0.0, (percent_there * 90.0), -5.0, 5.0, 0.0)
+          player.yawpitchroll(0.0, 0.0, (percent_there * 90.0), -half_size, half_size, 0.0)
         elsif move_vector[0] < 0
-          player.yawpitchroll(0.0, 0.0, percent_there * -90.0, 5.0, 5.0, 0.0)
+          player.yawpitchroll(0.0, 0.0, percent_there * -90.0, half_size, half_size, 0.0)
         elsif move_vector[1] > 0
-          player.yawpitchroll(0.0, (percent_there * -90.0), 0.0, 0.0, 5.0, -5.0)
+          player.yawpitchroll(0.0, (percent_there * -90.0), 0.0, 0.0, half_size, -half_size)
         elsif move_vector[1] < 0
-          player.yawpitchroll(0.0, (percent_there * 90.0), 0.0, 0.0, 5.0, 5.0)
+          player.yawpitchroll(0.0, (percent_there * 90.0), 0.0, 0.0, half_size, half_size)
         end
       else
         player.yawpitchroll(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
@@ -102,14 +104,14 @@ def snake(gl)
       snake.deltap(50.0, 0.0, 50.0)
       snake.yawpitchroll(0.0, global_time * 100.0, global_time * -100.0, 0.0, 0.0, 0.0)
 
-      player.draw(true)
+      player.draw(false)
       snake.draw(true)
 
-      gl.draw_grid(33, 10.0)
+      gl.draw_grid(33, size)
     }
 
-    #gl.twod {
-    #  gl.draw_fps(10, 10)
-    #}
+    gl.twod {
+      gl.draw_fps(10, 10)
+    }
   }
 end
