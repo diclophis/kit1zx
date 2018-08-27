25f3e63999a929d59050b18e7277f703a5cb45f5
Jon Bardin <diclophis@gmail.com>
2018-08-27 02:27:14 -0700

    better render loop

--- lib/kube.rb
+++ lib/kube.rb
@@ -7,8 +7,8 @@ if $0 # /usr/bin/ruby MRI ruby below
 
   class Kube
     def pod(*args)
-      name, latest_condition, phase, container_readiness, container_states, age, exiting = *args
-      $stdout.write([name, latest_condition, phase, container_readiness, container_states, age, exiting].to_msgpack)
+      name, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time = *args
+      $stdout.write([name, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time].to_msgpack)
       $stdout.flush
     end
 
@@ -17,8 +17,8 @@ if $0 # /usr/bin/ruby MRI ruby below
       name = description["metadata"]["name"]
       created_at = description["metadata"]["creationTimestamp"] ? DateTime.parse(description["metadata"]["creationTimestamp"]) : nil
       deleted_at = description["metadata"]["deletionTimestamp"] ? DateTime.parse(description["metadata"]["deletionTimestamp"]) : nil
-      age = Time.now - created_at.to_time
-      exiting = deleted_at ? (Time.now - deleted_at.to_time + description["metadata"]["deletionGracePeriodSeconds"]) : nil
+      exit_at = deleted_at ? (deleted_at.to_time).to_i : nil
+      grace_time = description["metadata"]["deletionGracePeriodSeconds"]
       meta_keys = description["metadata"].keys
 
       case kind
@@ -49,7 +49,7 @@ if $0 # /usr/bin/ruby MRI ruby below
             end
           end
 
-          pod(name, latest_condition, phase, ready, state_keys, age, exiting)
+          pod(name, latest_condition, phase, ready, state_keys, created_at.to_time.to_i, exit_at, grace_time)
 
         when "Service"
           puts [kind].inspect
@@ -100,13 +100,16 @@ if $0 # /usr/bin/ruby MRI ruby below
     end
 
     def get_yaml
-      foo = IO.popen("kubectl get --all-namespaces --include-uninitialized=true --watch=true --output=json pods")
+      foo = IO.popen("kubectl get --include-uninitialized=true --watch=true --output=json pods")
     end
   end
 
   Kube.new.ingest!
 else
   def kube(gl)
+    waiting_str = "waiting"
+    terminating_str = "terminating"
+
     pods = {}
     size = 20.0
     half_size = size / 2.0
@@ -124,7 +127,7 @@ else
 
         unpacked_length = MessagePack.unpack(all_to_consider) do |result|
           if result
-            name, latest_condition, phase, container_readiness, container_states, age, exiting = result
+            name, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time = result
             cube = nil
             unless existing_pod = pods[name]
               cube = Cube.new(size, size, size, 1.0)
@@ -132,7 +135,10 @@ else
             else
               cube = existing_pod[0]
             end
-            existing_pod = [cube, latest_condition, phase, container_readiness, container_states, age, exiting]
+
+            up_and_running = (container_states && container_states.values.all? { |v| v != waiting_str && v != terminating_str })
+
+            existing_pod = [cube, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time, up_and_running]
             pods[name] = existing_pod
           end
         end
@@ -144,25 +150,43 @@ else
     gl.main_loop { |gtdt|
       global_time, delta_time = gtdt
       next unless delta_time > 0.0
-      camera_x = Math.sin(global_time * 0.1) * 300.0
-      camera_y  = Math.cos(global_time * 0.1) * 300.0
+
+      tnow = Time.now.to_i
+
+      mark_for_recycle = []
+
+      camera_x = Math.sin(global_time * 0.1) * 250.0
+      camera_y = ((Math.cos(global_time * 0.5) * 30.0) + 100.0)
+      camera_z = Math.cos(global_time * 0.1) * 250.0
 
       gl.threed {
-        gl.lookat(1, camera_x, 300.0, camera_y, 0.0, 0.0, 1.0, 60.0)
-        gl.draw_grid(33, size)
-        pods.each { |key, val|
-          cube, latest_condition, phase, container_readiness, container_states, age, exiting = val
-          cube.draw(false)
+        #gl.lookat(0, camera_x, camera_y, camera_z, 0.0, 0.0, 1.0, 359.0)
+        gl.lookat(1, camera_x, camera_y, camera_z, 0.0, 0.0, 1.0, 60.0)
+        gl.draw_grid(33, size * 2.0)
 
-          if exiting
-            percent_exited = (exiting / 3.0)
-            if percent_exited > 0.5
+        pods.each { |name, val|
+          cube, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time, up_and_running = val
+
+          cube.draw(false) if up_and_running
+
+          if exit_at
+            in_delta_exit = exit_at - tnow
+            percent_exited = ((in_delta_exit) / (grace_time))
+
+            if percent_exited < 0.678
+              mark_for_recycle << name
+              percent_exited = 0.0
+            end
+
+            if percent_exited > 1.0
               percent_exited = 1.0
             end
 
-            cube.deltas(1.0 - percent_exited, 1.0 - percent_exited, 1.0 - percent_exited)
+            cube.deltas(percent_exited, percent_exited, percent_exited)
           else
-            percent_started = (age / 5.0)
+            age = tnow - created_at
+            percent_started = (age / 10.0)
+
             if percent_started > 1.0
               percent_started = 1.0
             end
@@ -171,14 +195,21 @@ else
           end
         }
       }
+
       gl.twod {
         gl.draw_fps(10, 10)
         pods.each { |name, val|
-          cube, latest_condition, phase, container_readiness, container_states, age, exiting = val
-          cube.label(name) unless exiting
+          cube, latest_condition, phase, container_readiness, container_states, created_at, exit_at, grace_time, up_and_running = val
+          cube.label(name) unless exit_at
         }
       }
+
       UV::run(UV::UV_RUN_NOWAIT)
+
+      mark_for_recycle.each do |name|
+        puts [:delete, name].inspect
+        pods.delete(name)
+      end
     }
 
     f.close
