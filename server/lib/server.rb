6094905fd56c871b809b5c5bb33fef298880b17b
Jon Bardin <diclophis@gmail.com>
2019-01-06 14:40:15 -0800

    why does this segfault??

--- server/lib/server.rb
+++ server/lib/server.rb
@@ -35,9 +35,9 @@ class Connection
   end
 
   def disconnect!
-    self.timer.stop
-    self.socket.unref
-    #$stdout.write("x")
+    self.timer.stop if self.timer
+    self.socket.unref if self.socket
+    log!("x")
   end
 
   def handle_bytes!(b)
@@ -46,63 +46,109 @@ class Connection
       offset = self.phr.parse_request(self.ss)
       case offset
       when Fixnum
-        #$stdout.write(self.phr.headers.inspect)
+        case phr.path
+        when "/"
+          filename = "/var/tmp/big.data"
+          f = UV::FS::open(filename, UV::FS::O_RDONLY, UV::FS::S_IREAD)
+          header = "HTTP/1.1 200 OK\r\nContent-Length: #{f.stat.size}\r\nTransfer-Coding: chunked\r\n\r\n"
+          self.socket.write(header)
 
-        #$did_handle_bytes += 1
+          #idle = UV::Idle.new
+          #idle.start do |x|
+          #  $stdout.write(".")
+          #  #$stdout.write(self.phr.headers.inspect)
+          #  #while f.active? && self.socket.active?
+          #  self.socket.write(f.read)
+          #  #end
+          #  #self.socket.close
+          #end
 
-        #TODO???
-        #unless WebSocket.create_accept(key).securecmp(phr.headers.to_h.fetch('sec-websocket-accept'))
-        #   raise Error, "Handshake failure"
-        #end
+          #f.sendfile(self.socket)
+          #UV::FS::sendfile(self.socket, f, 0, f.stat.size)
+          #UV::FS::O_RDONLY, UV::FS::S_IREAD)
 
-        sec_websocket_key = self.phr.headers.detect { |k,v|
-          k == "sec-websocket-key"
-        }[1]
+          file_size =  f.stat.size
+          sent = 0
 
-        #$stdout.write(sec_websocket_key)
+          while sent < file_size
+            left = file_size - sent
+            if left > 1024
+              left = 1024
+            end
 
-				self.write_ws_response!(sec_websocket_key)
+            UV::FS::sendfile(self.socket.fileno, f, sent, left)
+            #{
+            #  false
+            #}
 
-        self.timer = UV::Timer.new
-        self.timer.start(1000, 1000) {
-          #$stdout.write(".")
-          #$did_timer += 1
-          if @pos_t > 0
-            @pos_t *= -1
-            @pos_x += 1
-          else
-            @pos_t *= -1
-            @pos_y += 1
+            sent += left
           end
 
-          msg = MessagePack.pack({"globalPlayerLocation"=>{"X"=>@pos_x, "Y"=>@pos_y}})
-        #  #msg = ("cheese" * 1024)
-        #  #$stdout.write("doing #{msg.inspect} tick")
-          begin
-            self.client.queue_msg(msg, :binary_frame)
-            outg = self.client.send
-          rescue Wslay::Err => e
-            self.disconnect!
-          end
-        #  #$stdout.write("done tick #{outg.inspect}")
-        }
+          #self.socket.write(f.read) do |a|
+          #  $stdout.write(a.inspect)
+          #  #self.socket.close
+          #end
 
-        self.processing_handshake = false
-        self.last_buf = self.ss[offset..-1]
-        proto_ok = (self.client.recv != :proto)
-        unless proto_ok
-          #$stdout.write(:wslay_handshake_proto_error)
-          self.socket.close
+          log!("CHEESE", header.inspect)
+        else
+          #$stdout.write(self.phr.headers.inspect)
+
+          #$did_handle_bytes += 1
+
+          #TODO???
+          #unless WebSocket.create_accept(key).securecmp(phr.headers.to_h.fetch('sec-websocket-accept'))
+          #   raise Error, "Handshake failure"
+          #end
+
+          sec_websocket_key = self.phr.headers.detect { |k,v|
+            k == "sec-websocket-key"
+          }[1]
+
+          #$stdout.write(sec_websocket_key)
+
+          self.write_ws_response!(sec_websocket_key)
+
+          self.timer = UV::Timer.new
+          self.timer.start(1000, 1000) {
+            #$stdout.write(".")
+            #$did_timer += 1
+            if @pos_t > 0
+              @pos_t *= -1
+              @pos_x += 1
+            else
+              @pos_t *= -1
+              @pos_y += 1
+            end
+
+            msg = MessagePack.pack({"globalPlayerLocation"=>{"X"=>@pos_x, "Y"=>@pos_y}})
+          #  #msg = ("cheese" * 1024)
+          #  #$stdout.write("doing #{msg.inspect} tick")
+            begin
+              self.client.queue_msg(msg, :binary_frame)
+              outg = self.client.send
+            rescue Wslay::Err => e
+              self.disconnect!
+            end
+          #  #$stdout.write("done tick #{outg.inspect}")
+          }
+
+          self.processing_handshake = false
+          self.last_buf = self.ss[offset..-1]
+          proto_ok = (self.client.recv != :proto)
+          unless proto_ok
+            #$stdout.write(:wslay_handshake_proto_error)
+            self.socket.close
+          end
+
+          #$stdout.write("done handshake")
         end
-
-        #$stdout.write("done handshake")
       when :incomplete
         #$stdout.write("incomplete")
       when :parser_error
         #$stdout.write([:parser_error, offset].inspect)
       end
     else
-      #$stdout.write("doing non-handshake byte transfer\n")
+      log!("doing non-handshake byte transfer\n")
 
       self.last_buf = b
       proto_ok = (self.client.recv != :proto)
@@ -147,74 +193,75 @@ class Server
   def create_connection!
     nc = Connection.new
 
-    nc.wslay_callbacks = Wslay::Event::Callbacks.new
-
     nc.ss = ""
     nc.last_buf = nil
     nc.processing_handshake = true
 
-    nc.wslay_callbacks.recv_callback do |buf, len|
-      # when wslay wants to read data
-      # buf is a cptr, if your I/O gem can write to a C pointer you have to write at most len bytes into it
-      # and return the bytes written
-      # or else return a mruby String or a object which can be converted into a String via to_str
-      # and be up to len bytes long
-      # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when there is nothing to read
-      throw_away_buf = nc.last_buf.dup
-      nc.last_buf = nil
-      throw_away_buf
-    end
+    #nc.wslay_callbacks = Wslay::Event::Callbacks.new
 
-    #TODO: this is where the client msgs are recvd
-    nc.wslay_callbacks.on_msg_recv_callback do |msg|
-      # when a WebSocket msg is fully recieved this callback is called
-      # you get a Wslay::Event::OnMsgRecvArg Struct back with the following fields
-      # :rsv => reserved field from WebSocket spec, there are Wslay.get_rsv1/2/3 helper methods
-      # :opcode => :continuation_frame, :text_frame, :binary_frame, :connection_close, :ping or
-      # :pong, Wslay.is_ctrl_frame? helper method is provided too
-      # :msg => the message revieced
-      # :status_code => :normal_closure, :going_away, :protocol_error, :unsupported_data, :no_status_rcvd,
-      # :abnormal_closure, :invalid_frame_payload_data, :policy_violation, :message_too_big, :mandatory_ext,
-      # :internal_server_error, :tls_handshake
-      # to_str => returns the message revieced
-      if msg[:opcode] == :binary_frame
-        #self.feed_state!(msg[:msg])
-        #$stdout.write(msg[:msg].inspect)
+    #nc.wslay_callbacks.recv_callback do |buf, len|
+    #  # when wslay wants to read data
+    #  # buf is a cptr, if your I/O gem can write to a C pointer you have to write at most len bytes into it
+    #  # and return the bytes written
+    #  # or else return a mruby String or a object which can be converted into a String via to_str
+    #  # and be up to len bytes long
+    #  # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when there is nothing to read
+    #  throw_away_buf = nc.last_buf.dup
+    #  nc.last_buf = nil
+    #  throw_away_buf
+    #end
 
-        bytes = msg[:msg]
+    ##TODO: this is where the client msgs are recvd
+    #nc.wslay_callbacks.on_msg_recv_callback do |msg|
+    #  # when a WebSocket msg is fully recieved this callback is called
+    #  # you get a Wslay::Event::OnMsgRecvArg Struct back with the following fields
+    #  # :rsv => reserved field from WebSocket spec, there are Wslay.get_rsv1/2/3 helper methods
+    #  # :opcode => :continuation_frame, :text_frame, :binary_frame, :connection_close, :ping or
+    #  # :pong, Wslay.is_ctrl_frame? helper method is provided too
+    #  # :msg => the message revieced
+    #  # :status_code => :normal_closure, :going_away, :protocol_error, :unsupported_data, :no_status_rcvd,
+    #  # :abnormal_closure, :invalid_frame_payload_data, :policy_violation, :message_too_big, :mandatory_ext,
+    #  # :internal_server_error, :tls_handshake
+    #  # to_str => returns the message revieced
+    #  if msg[:opcode] == :binary_frame
+    #    #self.feed_state!(msg[:msg])
+    #    #$stdout.write(msg[:msg].inspect)
 
-        all_bits_to_consider = (@left_over_bits || "") + bytes
-        all_l = all_bits_to_consider.length
+    #    bytes = msg[:msg]
 
-        small_subset_to_consider = all_bits_to_consider[0, 40960]
-        considered_subset_length = small_subset_to_consider.length
+    #    all_bits_to_consider = (@left_over_bits || "") + bytes
+    #    all_l = all_bits_to_consider.length
 
-        unpacked_length = MessagePack.unpack(small_subset_to_consider) do |result|
-          log!(result) if result
-        end
+    #    small_subset_to_consider = all_bits_to_consider[0, 40960]
+    #    considered_subset_length = small_subset_to_consider.length
 
-        @left_over_bits = all_bits_to_consider[unpacked_length, all_l]
-      end
-    end
+    #    unpacked_length = MessagePack.unpack(small_subset_to_consider) do |result|
+    #      log!(result) if result
+    #    end
 
-    nc.wslay_callbacks.send_callback do |buf|
-      # when there is data to send, you have to return the bytes send here
-      # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when sending would block
-      #begin
-        nc.socket.try_write(buf)
-      #rescue UVError => e
-      #  #nc.disconnect!
-      #  raise Errno::EAGAIN
-      #  0
-      #end
-    end
+    #    @left_over_bits = all_bits_to_consider[unpacked_length, all_l]
+    #  end
+    #end
 
-    nc.client = Wslay::Event::Context::Server.new nc.wslay_callbacks
+    #nc.wslay_callbacks.send_callback do |buf|
+    #  # when there is data to send, you have to return the bytes send here
+    #  # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when sending would block
+    #  #begin
+    #    nc.socket.try_write(buf)
+    #  #rescue UVError => e
+    #  #  #nc.disconnect!
+    #  #  raise Errno::EAGAIN
+    #  #  0
+    #  #end
+    #end
+
+    #nc.client = Wslay::Event::Context::Server.new nc.wslay_callbacks
+
     nc.phr = Phr.new
 
     on_read_start = Proc.new { |b|
+      log!(:on_read_start, b)
       if b && b.is_a?(UVError)
-        log!(:on_read_start, b)
         nc.disconnect!
       else
         if b && b.is_a?(String)
