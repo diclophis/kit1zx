c3ca4f35f278b75d92462ad31b16260369839a1f
Jon Bardin <diclophis@gmail.com>
2019-01-06 17:29:40 -0800

    more cleanup

--- server/lib/server.rb
+++ server/lib/server.rb
@@ -21,17 +21,19 @@ $stdout.read_stop
 class Connection
   attr_accessor :last_buf,
                 :wslay_callbacks,
-                :client,
+                :ws,
                 :phr,
                 :processing_handshake,
                 :ss,
                 :socket,
                 :timer
 
-  def initialize
+  def initialize(socket)
     @pos_x = 0
     @pos_y = 0
     @pos_t = 1
+
+    self.socket = socket
 
     self.ss = ""
     self.last_buf = nil
@@ -133,8 +135,8 @@ class Connection
           #  #msg = ("cheese" * 1024)
           #  #$stdout.write("doing #{msg.inspect} tick")
             begin
-              self.client.queue_msg(msg, :binary_frame)
-              outg = self.client.send
+              self.ws.queue_msg(msg, :binary_frame)
+              outg = self.ws.send
             rescue Wslay::Err => e
               self.disconnect!
             end
@@ -143,7 +145,7 @@ class Connection
 
           self.processing_handshake = false
           self.last_buf = self.ss[offset..-1]
-          proto_ok = (self.client.recv != :proto)
+          proto_ok = (self.ws.recv != :proto)
           unless proto_ok
             #$stdout.write(:wslay_handshake_proto_error)
             self.socket.close
@@ -160,7 +162,7 @@ class Connection
       log!("doing non-handshake byte transfer\n")
 
       self.last_buf = b
-      proto_ok = (self.client.recv != :proto)
+      proto_ok = (self.ws.recv != :proto)
       unless proto_ok
         #$stdout.write("wslay_handshake_proto_error")
         self.socket.close
@@ -173,15 +175,15 @@ class Connection
 
     #B64.encode(Sysrandom.buf(16)).chomp!
     #The Sec-WebSocket-Accept part is interesting.
-    #The server must derive it from the Sec-WebSocket-Key that the client sent.
-    #To get it, concatenate the client's Sec-WebSocket-Key and "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" together
+    #The server must derive it from the Sec-WebSocket-Key that the ws sent.
+    #To get it, concatenate the ws's Sec-WebSocket-Key and "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" together
     #(it's a "magic string"), take the SHA-1 hash of the result, and return the base64 encoding of the hash.
 
     self.socket.write("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: #{key}\r\n\r\n")
     #self.socket.write("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n")
   end
 
-  def on_read(b)
+  def read_bytes_safely(b)
     if b && b.is_a?(UVError)
       self.disconnect!
     else
@@ -189,6 +191,68 @@ class Connection
         self.handle_bytes!(b)
       end
     end
+  end
+
+  def upgrade_to_websocket!
+    self.wslay_callbacks = Wslay::Event::Callbacks.new
+
+    self.wslay_callbacks.recv_callback do |buf, len|
+      # when wslay wants to read data
+      # buf is a cptr, if your I/O gem can write to a C pointer you have to write at most len bytes into it
+      # and return the bytes written
+      # or else return a mruby String or a object which can be converted into a String via to_str
+      # and be up to len bytes long
+      # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when there is nothing to read
+      throw_away_buf = self.last_buf.dup
+      self.last_buf = nil
+      throw_away_buf
+    end
+
+    #TODO: this is where the ws msgs are recvd
+    self.wslay_callbacks.on_msg_recv_callback do |msg|
+      # when a WebSocket msg is fully recieved this callback is called
+      # you get a Wslay::Event::OnMsgRecvArg Struct back with the following fields
+      # :rsv => reserved field from WebSocket spec, there are Wslay.get_rsv1/2/3 helper methods
+      # :opcode => :continuation_frame, :text_frame, :binary_frame, :connection_close, :ping or
+      # :pong, Wslay.is_ctrl_frame? helper method is provided too
+      # :msg => the message revieced
+      # :status_code => :normal_closure, :going_away, :protocol_error, :unsupported_data, :no_status_rcvd,
+      # :abnormal_closure, :invalid_frame_payload_data, :policy_violation, :message_too_big, :mandatory_ext,
+      # :internal_server_error, :tls_handshake
+      # to_str => returns the message revieced
+      if msg[:opcode] == :binary_frame
+        #self.feed_state!(msg[:msg])
+        #$stdout.write(msg[:msg].inspect)
+
+        bytes = msg[:msg]
+
+        all_bits_to_consider = (@left_over_bits || "") + bytes
+        all_l = all_bits_to_consider.length
+
+        small_subset_to_consider = all_bits_to_consider[0, 40960]
+        considered_subset_length = small_subset_to_consider.length
+
+        unpacked_length = MessagePack.unpack(small_subset_to_consider) do |result|
+          log!(result) if result
+        end
+
+        @left_over_bits = all_bits_to_consider[unpacked_length, all_l]
+      end
+    end
+
+    self.wslay_callbacks.send_callback do |buf|
+      # when there is data to send, you have to return the bytes send here
+      # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when sending would block
+      #begin
+        self.socket.try_write(buf)
+      #rescue UVError => e
+      #  #self.disconnect!
+      #  raise Errno::EAGAIN
+      #  0
+      #end
+    end
+
+    self.ws = Wslay::Event::Context::Server.new self.wslay_callbacks
   end
 end
 
@@ -214,73 +278,10 @@ class Server
   end
 
   def create_connection!
-    nc = Connection.new
+    http = Connection.new(@server.accept)
 
-    #nc.wslay_callbacks = Wslay::Event::Callbacks.new
-
-    #nc.wslay_callbacks.recv_callback do |buf, len|
-    #  # when wslay wants to read data
-    #  # buf is a cptr, if your I/O gem can write to a C pointer you have to write at most len bytes into it
-    #  # and return the bytes written
-    #  # or else return a mruby String or a object which can be converted into a String via to_str
-    #  # and be up to len bytes long
-    #  # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when there is nothing to read
-    #  throw_away_buf = nc.last_buf.dup
-    #  nc.last_buf = nil
-    #  throw_away_buf
-    #end
-
-    ##TODO: this is where the client msgs are recvd
-    #nc.wslay_callbacks.on_msg_recv_callback do |msg|
-    #  # when a WebSocket msg is fully recieved this callback is called
-    #  # you get a Wslay::Event::OnMsgRecvArg Struct back with the following fields
-    #  # :rsv => reserved field from WebSocket spec, there are Wslay.get_rsv1/2/3 helper methods
-    #  # :opcode => :continuation_frame, :text_frame, :binary_frame, :connection_close, :ping or
-    #  # :pong, Wslay.is_ctrl_frame? helper method is provided too
-    #  # :msg => the message revieced
-    #  # :status_code => :normal_closure, :going_away, :protocol_error, :unsupported_data, :no_status_rcvd,
-    #  # :abnormal_closure, :invalid_frame_payload_data, :policy_violation, :message_too_big, :mandatory_ext,
-    #  # :internal_server_error, :tls_handshake
-    #  # to_str => returns the message revieced
-    #  if msg[:opcode] == :binary_frame
-    #    #self.feed_state!(msg[:msg])
-    #    #$stdout.write(msg[:msg].inspect)
-
-    #    bytes = msg[:msg]
-
-    #    all_bits_to_consider = (@left_over_bits || "") + bytes
-    #    all_l = all_bits_to_consider.length
-
-    #    small_subset_to_consider = all_bits_to_consider[0, 40960]
-    #    considered_subset_length = small_subset_to_consider.length
-
-    #    unpacked_length = MessagePack.unpack(small_subset_to_consider) do |result|
-    #      log!(result) if result
-    #    end
-
-    #    @left_over_bits = all_bits_to_consider[unpacked_length, all_l]
-    #  end
-    #end
-
-    #nc.wslay_callbacks.send_callback do |buf|
-    #  # when there is data to send, you have to return the bytes send here
-    #  # the I/O object must be in non blocking mode and raise EAGAIN/EWOULDBLOCK when sending would block
-    #  #begin
-    #    nc.socket.try_write(buf)
-    #  #rescue UVError => e
-    #  #  #nc.disconnect!
-    #  #  raise Errno::EAGAIN
-    #  #  0
-    #  #end
-    #end
-
-    #nc.client = Wslay::Event::Context::Server.new nc.wslay_callbacks
-
-
-
-    nc.socket = @server.accept
-    nc.socket.read_start { |b|
-      nc.on_read(b)
+    http.socket.read_start { |b|
+      http.read_bytes_safely(b)
     }
   end
 
