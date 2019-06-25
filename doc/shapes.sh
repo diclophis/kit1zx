8dbe614e7a7d60a6492042b268eccc5338a7790e
Jon Bardin <diclophis@gmail.com>
2019-06-24 20:28:12 -0700

    without texture

--- doc/shapes.sh
+++ doc/shapes.sh
@@ -31,15 +31,20 @@ do
 
   #FOO="-m vn vc fc wt"
   #FOO="-om vc vf vq vn vt fc ff fq fn wc wn wt"
-  FOO="-om vc vf vq vn fc ff fq fn wc wn"
+  #FOO="-om vc vf vq vn fc ff fq fn"
+  #FOO="-om vc vf vq vn"
+  #FOO=""
+  #FOO="-om vc vf vq vn fc ff fq fn wc wn"
+  FOO="-om vc vf vn fc ff fq fn"
   MESHARGS="-i resources/shape-${I}.stl -o resources/shape-${I}.obj $FOO -s doc/openscad/$FILTERS"
-  MESHARGS_TWO="-i resources/shape-${I}-mid.obj -o resources/shape-${I}.obj $FOO -s doc/openscad/foop.mlx"
+  
+  #MESHARGS_TWO="-i resources/shape-${I}-mid.obj -o resources/shape-${I}.obj $FOO -s doc/openscad/foop.mlx"
 
   #MESHARGS="-d /var/tmp/d.log -l /var/tmp/l.log -i resources/shape-${I}.stl -o resources/shape-${I}.obj $FOO"
 
   $MESHLAB ${MESHARGS}
 
-  cp resources/shape-null_tex.png resources/shape-${I}_tex.png
+  #cp resources/shape-null_tex.png resources/shape-${I}_tex.png
 
   sed -i -e "s~mtllib\ \./~mtllib\ $THIS_WORKING_DIR/resources/~" resources/shape-${I}.obj 
   sed -i -e "s~map_Kd\ ~map_Kd\ $THIS_WORKING_DIR/resources/~" resources/shape-${I}.obj.mtl 
